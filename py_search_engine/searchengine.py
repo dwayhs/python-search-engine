e31e9dd414c3046571d23fe4b4a0faf66f12c6ad
dwayhs <danielwayhs@gmail.com>
2017-03-14 15:25:34 -0300

    refactor: Moving SearchIndex class to the core package and renaming to Index

--- py_search_engine/searchengine.py
+++ py_search_engine/searchengine.py
@@ -34,37 +34,6 @@ class SimpleTokenizer(BaseFilter):
         return term.split(' ')
 
 
-class SearchIndex:
-    def __init__(self, terms_store, mapping={}):
-        self.terms_store = terms_store
-        self.mapping = mapping
-
-    def index(self, document):
-        document_terms = self._extract_terms_from_document(document)
-
-        self._store(document_terms, document)
-
-    def search(self, attribute, search_string):
-        attribute_analyzer = self.mapping[attribute]
-
-        search_terms = attribute_analyzer.analyze(search_string)
-
-        return self.terms_store.search(search_terms)
-
-    def _extract_terms_from_document(self, document):
-        terms = []
-
-        for attribute_name, attribute_analyzer in self.mapping.items():
-            document_attribute_value = document[attribute_name]
-
-            terms += attribute_analyzer.analyze(document_attribute_value)
-
-        return terms
-
-    def _store(self, terms, document):
-        self.terms_store.index(terms, document)
-
-
 class SimpleAnalyzer:
     PIPELINE = [
         SimpleNormalizeFilter,
